b0b8914aaa4dbcbdf9385598f26320f511e816da
BaeNouvelleVie <i202122429@cibertec.edu.pe>
2024-07-02 14:29:51 -0500

    inicio

---
+++ src/main/java/pe/edu/cibertec/DSWII_T3_KAPUSTIN_PEREZ_KEVIN_JOEL/DswiiT3KapustinPerezKevinJoelApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_T3_KAPUSTIN_PEREZ_KEVIN_JOEL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiT3KapustinPerezKevinJoelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiT3KapustinPerezKevinJoelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DSWII_T3_KAPUSTIN_PEREZ_KEVIN_JOEL/DswiiT3KapustinPerezKevinJoelApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_T3_KAPUSTIN_PEREZ_KEVIN_JOEL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiT3KapustinPerezKevinJoelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
